9f2f88716587f838538749a94b7c45231bd23f38
Sergey Yakunin <sergey@yakunins.com>
2024-11-19 21:25:35 -0800

    blink → blinkOptions prop rename

--- src/BlinkingDigit/BlinkingDigit.tsx
+++ src/BlinkingDigit/BlinkingDigit.tsx
@@ -1,33 +1,43 @@
 import React, { useState, useEffect } from 'react';
+import { default as cx } from 'classnames';
+
 import { Digit } from '../Digit';
 import { Blinker } from '../Blinker';
 
 type BlinkingDigit = Digit & {
-  blink?: {
+  blinkOptions?: {
     period?: Blinker['period'];
     ratio?: Blinker['ratio'];
   };
 };
 
-const digitBlinker = new Blinker(); // singleton makes all BlinkingDigit's to blink synchronoulsy
+const blinker = new Blinker(); // singleton makes all BlinkingDigit's to blink synchronoulsy
 
-export const BlinkingDigit = React.memo(({ blink, ...rest }: BlinkingDigit) => {
-  if (
-    blink?.period === 0 ||
-    blink?.ratio === 0 ||
-    typeof rest?.off === 'boolean'
-  )
-    return <Digit {...rest} />; // no blinker required
+export const BlinkingDigit = React.memo(
+  ({ blinkOptions, className, ...rest }: BlinkingDigit) => {
+    const [visible, setVisible] = useState(blinker.visible);
 
-  const [visible, setVisible] = useState(digitBlinker.visible);
+    useEffect(() => {
+      if (blinkOptions?.period) blinker.period = blinkOptions.period;
+      if (blinkOptions?.ratio) blinker.ratio = blinkOptions.ratio;
 
-  useEffect(() => {
-    if (blink?.period) digitBlinker.period = blink.period;
-    if (blink?.ratio) digitBlinker.ratio = blink.ratio;
+      blinker.subscribe(setVisible);
+      return () => blinker.unsubscribe(setVisible);
+    }, [blinkOptions?.period, blinkOptions?.ratio]);
 
-    digitBlinker.subscribe(setVisible);
-    return () => digitBlinker.unsubscribe(setVisible);
-  }, [blink?.period, blink?.ratio]);
+    if (
+      blinkOptions?.period === 0 ||
+      blinkOptions?.ratio === 0 ||
+      typeof rest?.off === 'boolean'
+    )
+      return <Digit {...rest} className={cx('blinking', className)} />; // no blinker required
 
-  return <Digit {...rest} off={rest.off || !visible} />;
-});
+    return (
+      <Digit
+        {...rest}
+        className={cx('blinking', className)}
+        off={rest.off || !visible}
+      />
+    );
+  }
+);
